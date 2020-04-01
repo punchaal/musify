424dd0309c29636b5b9b0633abc22f617838b464
Pratik <punchaal@gmail.com>
2020-03-31 20:23:08 -0400

    Added the front-end for the landing page

--- musify-client/src/App.js
+++ musify-client/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import SignIn from './pages/login.page';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#86b72a'
+    },
+    contrastText: 'white',
+    secondary: {
+      main: '#86b72a'
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <SignIn />
+    </MuiThemeProvider>
   );
 }
 
