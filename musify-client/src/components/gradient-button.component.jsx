4c0f9ff03d918cd4fa7ff7196a4e03a220c633be
rajavi <rajavi.shah16@gmail.com>
2020-04-05 18:49:37 -0400

    Changed gradient button shadow

--- musify-client/src/components/gradient-button.component.jsx
+++ musify-client/src/components/gradient-button.component.jsx
@@ -13,7 +13,7 @@ const styles = {
     color: 'white',
     height: 48,
     padding: '0 30px',
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    boxShadow: '0 3px 5px 2px rgba(90, 173, 105, .3)',
   },
 };
 
