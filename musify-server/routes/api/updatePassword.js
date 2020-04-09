37f3f440819c84cc6b8d6194778a4f441877b418
Pratik <punchaal@gmail.com>
2020-04-09 11:49:13 -0400

    Added backend validation for updated password

--- musify-server/routes/api/updatePassword.js
+++ musify-server/routes/api/updatePassword.js
@@ -5,34 +5,43 @@ const bcrypt = require('bcryptjs');
 // @route     GET api/reset
 // @desc      Test route
 // @access    Public
-router.put('/', async (req, res) => {
-  try {
-    let user = await User.findOne({
-      email: req.body.username,
-    });
-
-    if (user !== null) {
-      console.log('User exists in db');
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(req.body.password, salt);
-
-      await user.update({
-        password: user.password,
-        resetPasswordToken: null,
-        resetPasswordExpires: null,
+router.put(
+  '/',
+  [
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      let user = await User.findOne({
+        email: req.body.username,
       });
 
-      console.log('password updated');
-      res.status(200).send({ message: 'Password updated' });
-    } else {
-      console.log('No user exists in the database to update');
-      res.status(404).json('No user exists in the database to update');
+      if (user !== null) {
+        console.log('User exists in db');
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(req.body.password, salt);
+
+        await user.update({
+          password: user.password,
+          resetPasswordToken: null,
+          resetPasswordExpires: null,
+        });
+
+        console.log('password updated');
+        res.status(200).send({ message: 'Password updated' });
+      } else {
+        console.log('No user exists in the database to update');
+        res.status(404).json('No user exists in the database to update');
+      }
+    } catch (err) {
+      console.error(error.message);
+      res.status(500).send('Server error');
     }
-  } catch (err) {
-    console.error(error.message);
-    res.status(500).send('Server error');
   }
-});
+);
 
 module.exports = router;
