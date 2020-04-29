f9dc166ec2f02cbcac5674b459d0b05fb7960fd3
Pratik <punchaal@gmail.com>
2020-04-29 11:46:04 -0400

    Added redis cloud integration

--- musify-server/src/redis-local.js
+++ musify-server/src/redis-local.js
@@ -1,6 +1,18 @@
 const redis = require('async-redis');
-const REDIS_PORT = process.env.PORT || 6379;
+require('dotenv').config();
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_HOST = process.env.REDIS_HOST;
+const redisAuth = process.env.REDIS_PASS;
 
-const client = redis.createClient(REDIS_PORT);
+const client = redis.createClient({
+  port: REDIS_PORT,
+  host: REDIS_HOST,
+});
+
+client.auth(redisAuth, function (err, response) {
+  if (err) {
+    throw err;
+  }
+});
 
 module.exports = client;
