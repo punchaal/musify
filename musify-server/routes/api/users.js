e894b5bfdfec7d563f76bd1c0c8bc4a083dc844c
Pratik <punchaal@gmail.com>
2020-04-30 11:08:23 -0400

    Testing route and deploy

--- musify-server/routes/api/users.js
+++ musify-server/routes/api/users.js
@@ -6,6 +6,7 @@ const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
+//Test
 
 // @route     POST api/users
 // @desc      Register user
