8eaec2c4da1b4e3fe16c31ad7fe2d33c3d202002
Pratik <punchaal@gmail.com>
2020-05-10 18:44:07 -0400

    Fixed the embed code bug

--- musify-client/src/components/post/spotify-embed..component.jsx
+++ musify-client/src/components/post/spotify-embed..component.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment, useState } from "react";
-import Skeleton from "@material-ui/lab/Skeleton";
+import React, { Fragment, useState } from 'react';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 export default function SpotifyEmbed(props) {
   const [loading, setLoading] = useState(true);
@@ -9,23 +9,24 @@ export default function SpotifyEmbed(props) {
   };
 
   const visible = {
-    display: loading ? "none" : "block",
+    display: loading ? 'none' : 'block',
   };
   return (
     <Fragment>
       {loading ? (
-        <Skeleton variant="rect" width={400} height={80} animation="wave" />
+        <Skeleton variant='rect' width={400} height={80} animation='wave' />
       ) : null}
       <iframe
-        src={`https://open.spotify.com/embed/track/${
-          props.post.uri && props.post.uri.slice(14, 36)
-        }`}
-        width="400"
-        height="80"
-        frameBorder="0"
-        allowtransparency="true"
-        allow="encrypted-media"
-        title="song"
+        src={
+          props.post.uri &&
+          `https://open.spotify.com/embed/track/${props.post.uri.slice(14, 36)}`
+        }
+        width='400'
+        height='80'
+        frameBorder='0'
+        allowtransparency='true'
+        allow='encrypted-media'
+        title='song'
         onLoad={hideSpinner}
         style={visible}
       ></iframe>
